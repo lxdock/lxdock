0d77613adc75d5823f19313d7bceff5c6a360dd8
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2017-01-04 12:45:36 -0500

    Don't use st_dev to compute project IDs as it can change

--- nomad/utils/identifier.py
+++ nomad/utils/identifier.py
@@ -6,4 +6,4 @@ import os
 def folderid(path):
     """ Computes a unique identifer using a folder path. """
     stats = os.stat(path)
-    return '{dev}{ino}'.format(dev=stats.st_dev, ino=stats.st_ino)
+    return str(stats.st_ino)
