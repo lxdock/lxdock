8a858d3bb08f13b55ace4b407c226811e8a02508
Morgan Aubert <morgan.aubert@savoirfairelinux.com>
2017-02-21 17:56:16 -0500

    Added tests for debian and archlinux guest classes

---
+++ tests/unit/guests/test_archlinux.py
@@ -0,0 +1,14 @@
+import unittest.mock
+
+from nomad.guests import ArchLinuxGuest
+
+
+class TestArchLinuxGuest:
+    def test_can_install_barebones_packages(self):
+        lxd_container = unittest.mock.Mock()
+        lxd_container.execute.return_value = ('ok', 'ok', '')
+        guest = ArchLinuxGuest(lxd_container)
+        guest.install_barebones_packages()
+        assert lxd_container.execute.call_count == 1
+        assert lxd_container.execute.call_args[0] == \
+            (['pacman', '-S', '--noconfirm'] + ArchLinuxGuest.barebones_packages, )
---
+++ tests/unit/guests/test_debian.py
@@ -0,0 +1,14 @@
+import unittest.mock
+
+from nomad.guests import DebianGuest
+
+
+class TestDebianGuest:
+    def test_can_install_barebones_packages(self):
+        lxd_container = unittest.mock.Mock()
+        lxd_container.execute.return_value = ('ok', 'ok', '')
+        guest = DebianGuest(lxd_container)
+        guest.install_barebones_packages()
+        assert lxd_container.execute.call_count == 1
+        assert lxd_container.execute.call_args[0] == \
+            (['apt-get', 'install', '-y'] + DebianGuest.barebones_packages, )
