76dc869a17f0348c291b2b651d95fc4b9051a0be
Morgan Aubert <morgan.aubert@zoho.com>
2017-07-29 17:43:59 -0400

    Fixed lint errors

--- lxdock/guests/__init__.py
+++ lxdock/guests/__init__.py
@@ -5,13 +5,13 @@
     side (the containers).
 """
 
-from .alpine import *  # noqa: F401, F403
-from .archlinux import *  # noqa: F401, F403
-from .base import *  # noqa: F401, F403
-from .centos import *  # noqa: F401, F403
-from .debian import *  # noqa: F401, F403
-from .fedora import *  # noqa: F401, F403
-from .gentoo import *  # noqa: F401, F403
-from .opensuse import *  # noqa: F401, F403
-from .oracle import *  # noqa: F401, F403
-from .ubuntu import *  # noqa: F401, F403
+from .alpine import *  # noqa
+from .archlinux import *  # noqa
+from .base import *  # noqa
+from .centos import *  # noqa
+from .debian import *  # noqa
+from .fedora import *  # noqa
+from .gentoo import *  # noqa
+from .opensuse import *  # noqa
+from .oracle import *  # noqa
+from .ubuntu import *  # noqa
